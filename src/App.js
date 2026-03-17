c2342195319d3286b0a5b06ca2492794431105b5
ezc112233 <emiliozapata590@gmail.com>
2026-03-17 08:27:40 -0300

    commit fix frontend 3

--- src/App.js
+++ src/App.js
@@ -31,27 +31,3 @@ function App() {
 }
 
 export default App;
-
-// import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
-// import "bootstrap-icons/font/bootstrap-icons.css";
-// import Login from "./components/login";
-// import Register from "./components/register";
-// import GameMasterPage from "./pages/GMpage";
-// import PersonajePage from "./pages/personajePage";
-// import "./App.css";
-
-// function App() {
-//     return (
-//         <>
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<Home />}/>
-//                 <Route path="/login" element={<Login/>}/>
-//                 <Route path="/register" element={<Register/>}/>
-//                 <Route path="/gm-page/*" element={<GameMasterPage/>}/>
-//                 <Route path="/personajes" element={<PersonajePage/>}/>
-//             </Routes>
-//         </Router>
-//         </>
