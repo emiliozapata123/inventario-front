176a42c5a240d7082153bb96d127834b26568365
ezc112233 <emiliozapata590@gmail.com>
2026-03-17 08:57:57 -0300

    commit frontend 4

--- src/components/activos/SelectProducto.jsx
+++ src/components/activos/SelectProducto.jsx
@@ -13,12 +13,16 @@ const SelectProducto = ({setFomulario, formulario, productos}) => {
                             className="btn btn-outline-primary w-100 dropdown-toggle" 
                             data-bs-toggle="dropdown" 
                         >
-                            {!formulario?.activo
-                            ? "Seleccionar Producto"
-                            :`${producto?.tipoProducto} 
-                            - ${!producto?.descripcion?"---":producto.descripcion}
-                            - ${!producto?.marca?"---":producto.marca}
-                            - ${!producto.modelo?"---":producto.modelo}`}
+                            {!formulario?.activo ? (
+                                "Seleccionar Producto"
+                            ):(
+                                <>
+                                {producto?.tipoProducto} {""} 
+                                - {!producto?.descripcion?<span className="text-muted">sin descripcion</span>:producto.descripcion} {""} 
+                                - {!producto?.marca?<span className="text-muted">sin marca</span>:producto.marca} {""} 
+                                - {!producto.modelo?<span className="text-muted">sin modelo</span>:producto.modelo}
+                                </>
+                            )}
                         </button>
 
                         <ul className="dropdown-menu w-100 dropdown-scroll">
@@ -33,10 +37,10 @@ const SelectProducto = ({setFomulario, formulario, productos}) => {
                                             }))
                                         }
                                     >
-                                        {p.tipoProducto} 
-                                        - {!p.descripcion?"---":p.descripcion} 
-                                        - {!p.marca?"---":p.marca} 
-                                        - {!p.modelo?"---":p.modelo}
+                                        {p.tipoProducto} {""}
+                                        - {!p.descripcion?<span className="text-muted">sin descripcion</span>:p.descripcion} {""}
+                                        - {!p.marca?<span className="text-muted">sin marca</span>:p.marca} {""}
+                                        - {!p.modelo?<span className="text-muted">sin modelo</span>:p.modelo}
                                     </button>
                                 </li>
                             ))}
